31a7d248a40bbd5d405518664638eec91fb72c72
Johan Bijlsma <johan.bijlsma24@gmail.com>
2020-05-15 07:26:06 +0200

    changed the footer

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -40,7 +40,17 @@
             <li class="link"><a href="#simpleDemo">Simple Demo</a></li>
         </ul>
         </aside>
-<footer><p class="footer__content">2020 - Personal project by @johanbijlsma</p>
+<footer>
+<p class="footer__content">2020 - Personal project by Johan Bijlsma
+</p>
+<div class="footer__socials">
+<a href="https://twitter.com/johanbijlsma"> <img src="/icon-social-twitter-white.svg" alt="Twitter Logo"/>
+<span class="footer__socials--text"> Johan's Twitter </span></a>
+<a href="https://github.com/johanbijlsma"> <img src="/Github.svg" alt="Github Logo"/>
+<span class="footer__socials--text"> Johan's Github </span></a>
+<a href="https://codepen.io/johanbijlsma/"> <img src="/codepen.svg" alt="codepen Logo"/>
+<span class="footer__socials--text"> Johan's Codepen </span></a>
+</div>
 </footer>
 
 
@@ -101,6 +111,42 @@
     .footer__content{
           grid-column-start: 2;
           text-align: center;
+    }
+    .footer__socials{
+        display: flex;
+        justify-content: space-evenly;
+        max-width: 60vw;
+        margin: 10px auto;
+        }
+    .footer__socials a img{
+        height: 30px;
+        transition: all 800ms 30ms cubic-bezier(0.165, 0.84, 0.44, 1);
+    }
+    .footer__socials a{
+        height: 30px;
+        width: fit-content;
+        padding: 10px;
+        transition: all 800ms 30ms cubic-bezier(0.165, 0.84, 0.44, 1);
+    }
+
+    .footer__socials a:hover, .footer__socials a:focus{
+        background: lightslategrey;
+        padding: 10px 30px;
+        transition: all 800ms 120ms ease;
+        font-weight: 700;
+        padding-bottom: 40px;
+        box-shadow: 0 3px 0 #8AC640;
+    }
+    .footer__socials a:hover img, .footer__socials a:focus img{
+        transform: scale(1.25);
+        padding-right: 12px;
+        transition: all 800ms 30ms cubic-bezier(0.165, 0.84, 0.44, 1);
+
+    }
+    .footer__socials a span.footer__socials--text{
+        transform: translate(0px, -10px);
+        display: inline-block;
+        color: #8AC640;
     }
 
 	@media (min-width: 640px) {
