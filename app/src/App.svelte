2b6eacf4d5126d5883675cae15e53486a106d655
Johan Bijlsma <johan.bijlsma24@gmail.com>
2020-05-19 12:48:24 +0200

    added more examples

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,114 +1,134 @@
+<script>
+  import Footer from "./Footer.svelte";
+  import Demo1 from "./Demo1.svelte";
+  import Demo2 from "./Demo2.svelte";
+  import Demo3 from "./Demo3.svelte";
+  export let title;
+
+  // console.log(container);
+</script>
+
 <div class="container">
-<header>
-			<h1>{title}</h1>
-            <img src="/logo-man.svg" class="logo" alt="Logo GSAP - superhero" height="100%">
-		</header>
-		<main>
-			<h2 id="Whatis">What's Greensock and what is GSAP?</h2>
-			<p>
-				Greensock is a JavaScript platform for animating DOM elements, and gives
-				the developer a lot more control over the animations.
-				<acronym title="Greensock Animation Platform">GSAP</acronym> is the name
-				of the platform, which stands for <strong>G</strong>reen<strong>S</strong>ock <strong>A</strong>nimation <strong>P</strong>latform
-			</p>
+  <header>
+    <h1>{title}</h1>
+    <img
+      src="/logo-man.svg"
+      class="logo"
+      alt="Logo GSAP - superhero"
+      height="100%"
+    />
+  </header>
+  <main>
+    <h2 id="Whatis">What's Greensock and what is GSAP?</h2>
+    <p>
+      Greensock is a JavaScript platform for animating DOM elements, and gives
+      the developer a lot more control over the animations.
+      <acronym title="Greensock Animation Platform">GSAP</acronym>
+      is the name of the platform, which stands for
+      <strong>G</strong>
+      reen
+      <strong>S</strong>
+      ock
+      <strong>A</strong>
+      nimation
+      <strong>P</strong>
+      latform
+    </p>
 
-<Demo1/>
-<Demo2/>
-		</main>
-        <aside>
-        <ul>
-            <li class="link"><a href="#Whatis"> What's Greensock and what is GSAP? </a></li>
-            <li class="link"><a href="#simpleDemo">Simple Demo</a></li>
-        </ul>
-        </aside>
-<Footer/>
-
+    <Demo1 />
+    <Demo2 />
+    <Demo3 />
+  </main>
+  <aside>
+    <ul class="content">
+      <li class="link">
+        <a href="#Whatis">What's Greensock and what is GSAP?</a>
+      </li>
+      <li class="link">
+        <a href="#simpleDemo">Simple Demo</a>
+      </li>
+      <li class="link">
+        <a href="#simpleDemo2">Demo with multiple items</a>
+      </li>
+    </ul>
+  </aside>
+  <Footer />
 
 </div>
+
 <style>
-
-
-.container{
+  .container {
     display: grid;
     grid-template-columns: 40px 40px auto 250px 40px;
     grid-template-rows: auto auto 100px;
-}
-	main {
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-        background-color: lightslategrey;
-        color: whitesmoke;
-        grid-column-start: 3;
-        grid-column-end: 4;
-        grid-row-start: 2;
-        grid-row-end: 2;
-        height: auto;
-	}
+  }
+  main {
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    background-color: lightslategrey;
+    color: whitesmoke;
+    grid-column-start: 3;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 2;
+    height: auto;
+  }
 
-    header{
-        margin: 0;
-        padding: 20px;
-        display: grid;
-        grid-column-start: 1;
-        grid-column-end: 6;
-        grid-row-start: 1;
-        grid-row-end: 1;
+  header {
+    margin: 0;
+    padding: 20px;
+    display: grid;
+    grid-column-start: 1;
+    grid-column-end: 6;
+    grid-row-start: 1;
+    grid-row-end: 1;
+  }
+  h1 {
+    margin: 0;
+  }
+  img.logo {
+    grid-column-start: 4;
+    grid-column-end: 4;
+    grid-row-start: 1;
+    grid-row-end: 1;
+  }
+  aside {
+    grid-column-start: 4;
+    grid-column-end: 5;
+    grid-row-start: 2;
+    grid-row-end: 3;
+    background-color: #8ac640;
+    columns: #333333;
+  }
+
+  aside .content {
+    position: sticky;
+    top: 100px;
+    list-style: none outside;
+    padding-inline-start: 10px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
     }
-	h1 {
-        margin: 0;
-	}
-    img.logo{
-        grid-column-start: 4;
-        grid-column-end: 4;
-        grid-row-start: 1;
-        grid-row-end: 1;
-    }
-    aside{
-        grid-column-start: 4;
-        grid-column-end: 5;
-        grid-row-start: 2;
-        grid-row-end: 3;
-        background-color: #8AC640;
-        columns: #333333;
-    }
+  }
 
+  * {
+    box-sizing: border-box;
+    font-family: "Jost", Verdana, sans-serif;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @font-face {
+    font-family: Jost;
+    src: url("/Jost-VariableFont_ital,wght.ttf");
+  }
 
-    * {
-	box-sizing: border-box;
-	font-family: 'Jost', Verdana, sans-serif;
-    }
+  header {
+    color: #8ac640;
 
-    @font-face {
-        font-family: Jost;
-        src: url("/Jost-VariableFont_ital,wght.ttf");
-    }
-
-    header{
-    color: #8AC640;
-
-        height: 120px;
-        background: #333333;
-    }
-
-
-
+    height: 120px;
+    background: #333333;
+  }
 </style>
-
-<script>
-
-import  Footer  from "./Footer.svelte";
-import  Demo1  from "./Demo1.svelte";
-import  Demo2  from "./Demo2.svelte";
-	export let title;
-
-
-    // console.log(container);
-
-</script>
--- app/src/Demo2.svelte
+++ app/src/Demo2.svelte
@@ -1,95 +1,159 @@
-<section>
-<h2>And a second simple animation</h2>
-<p>Again using <code>gsap.to</code>, but now with multiple items</p>
-<div class="demo__container" id="simpleDemo2">
-<h2 class="demo__title">Demo Multiple items: <code>gsap.to</code></h2>
-{#if !example2Started}
-<button class="button start" on:click={startAnimation} >
-	Start &RightAngleBracket;
-</button>
-{:else}
-<button class="button reset" on:click={resetAnimation} >
-	Reset &ldca;
-</button>
-{/if}
-<div class="box red"></div>
-<div class="box yellow"></div>
-<div class="box blue"></div>
+<script>
+  export let example2Started = false;
+  export let showCode = false;
+  function startAnimation(event) {
+    gsap.to(".red", { duration: 1, x: 100 });
+    gsap.to(".yellow", { duration: 1, delay: 1, x: 300, rotate: 720 });
+    gsap.to(".blue", {
+      duration: 1,
+      delay: 2,
+      scale: 2,
+      backgroundColor: "turquoise",
+    });
+    example2Started = true;
+  }
+  function resetAnimation(event) {
+    gsap.to(".box", { duration: 1, x: 0, scale: 1, rotate: 0 });
+    gsap.to(".blue", { backgroundColor: "blue" });
+    example2Started = false;
+  }
 
-</div>
+  export let codeSnippetHTML = `
+    <div class="box red"></div>
+    <div class="box yellow"></div>
+    <div class="box blue"></div>
+    `;
+
+  export let codeSnippetJS = `
+    gsap.to('.red', { duration: 1, x: 100 });
+    gsap.to('.yellow', { duration: 1, delay: 1, x: 300, rotate: 720 });
+    gsap.to('.blue', { duration: 1, delay: 2, scale: 2, backgroundColor: 'turquoise' });`;
+</script>
+
+<section>
+  <h2>And a second simple animation</h2>
+  <p>
+    Again using
+    <code>gsap.to</code>
+    , but now with multiple items
+  </p>
+  <div class="demo__container" id="simpleDemo2">
+    <h2 class="demo__title">
+      Demo Multiple items:
+      <code>gsap.to</code>
+    </h2>
+    <div class="show-code-container">
+      <label for="show-code-check">Show Code</label>
+      <input id="show-code-check" type="checkbox" bind:checked={showCode} />
+    </div>
+    <div class="demoblock">
+      {#if !example2Started}
+        <button class="button start" on:click={startAnimation}>
+          Start &RightAngleBracket;
+        </button>
+      {:else}
+        <button class="button reset" on:click={resetAnimation}>
+          Reset &ldca;
+        </button>
+      {/if}
+      <div class="box red" />
+      <div class="box yellow" />
+      <div class="box blue" />
+    </div>
+    {#if showCode}
+      <div class="codeblock">
+        HTML
+        <pre class="language-html">
+          <!-- <code class="language-javascript"> -->
+          {codeSnippetHTML}
+          <!-- </code> -->
+        </pre>
+        javaScript
+        <pre class="language-js">
+          <!-- <code class="language-javascript"> -->
+          {codeSnippetJS}
+          <!-- </code> -->
+        </pre>
+      </div>
+    {/if}
+  </div>
 </section>
 
 <style>
-.demo__container{
-        margin: 10px auto;
-        width: 80%;
-        border: dashed 2px rgba(0,0,30,0.3);
-        padding: 20px;
-        background-color: #33333375;
-        border-radius: 30px;
-    }
+  .demo__container {
+    margin: 10px auto;
+    width: 80%;
+    border: dashed 2px rgba(0, 0, 30, 0.3);
+    padding: 20px;
+    background-color: #33333375;
+    border-radius: 30px;
+    position: relative;
+  }
 
-    .demo__title{
-            color: #8AC640;
+  .demo__title {
+    color: #8ac640;
+    margin: 0 0 6px;
+  }
 
-    }
+  .box {
+    width: 35px;
+    height: 35px;
+    border-radius: 10px;
+    border: 2px solid white;
+  }
+  .box.red {
+    background-color: red;
+  }
+  .box.yellow {
+    background-color: yellow;
+  }
+  .box.blue {
+    background-color: blue;
+  }
 
-    .box{
-        width: 35px;
-        height: 35px;
-        border-radius: 10px;
-        border: 2px solid white;
-    }
-    .box.red{
-        background-color: red;
-    }
-    .box.yellow{
-        background-color: yellow;
-    }
-    .box.blue{
-        background-color: blue;
-    }
-
-    code{
+  code {
     font-family: monospace;
-    color: #8AC640;
-}
+    color: #8ac640;
+  }
+  pre ~ code {
+    color: white;
+  }
 
-button.button{
+  button.button {
     border-radius: 10px;
     border: 1px solid darkblue;
     padding: 8px 32px;
     box-shadow: inset 0 0 0 transparent;
-}
-.button.start:hover{
-    box-shadow: inset 0 -6px 1px #8AC640;
+  }
+  .button.start:hover {
+    box-shadow: inset 0 -6px 1px #8ac640;
     cursor: pointer;
     transition: all 200ms ease-in-out;
-    color: #8AC640;
-}
+    color: #8ac640;
+  }
 
-.button.reset{
+  .button.reset {
     box-shadow: inset 0 -40px 1px rgb(204, 63, 8);
     transition: all 200ms ease-in-out;
     color: #fff;
-    }
+  }
 
-    .button.reset:hover{
-    box-shadow: inset 0 -6px 1px #8AC640;
+  .button.reset:hover {
+    box-shadow: inset 0 -6px 1px #8ac640;
     cursor: pointer;
     transition: all 200ms ease-in-out;
-    color: #8AC640;
-}</style>
+    color: #8ac640;
+  }
 
-    <script>
-     export let example2Started = false;
-	function startAnimation(event) {
-    gsap.to('.red', { duration: 1, x: 100 });
-    gsap.to('.yellow', { duration: 1, delay: 1, x: 700, rotate: 720 });
-    gsap.to('.blue', { duration: 1, delay: 2, scale: 2 });
-    example2Started = true;
-	}
-	function resetAnimation(event) {
-    gsap.to('.box', { duration: 1, x: 0, scale: 1, rotate: 0 });
-    example2Started = false;
-	}</script>
+  .show-code-container {
+    position: absolute;
+    top: 21px;
+    right: 0;
+    display: inline-block;
+    min-width: 120px;
+  }
+
+  .show-code-container label {
+    display: inline-block;
+  }
+</style>
