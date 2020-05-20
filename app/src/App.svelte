0d92de72d9b5865c73acb9285ea5a44c2157f9bb
Johan Bijlsma <johan.bijlsma24@gmail.com>
2020-05-20 16:20:54 +0200

    added a first timeline

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -3,6 +3,7 @@
   import Demo1 from "./Demo1.svelte";
   import Demo2 from "./Demo2.svelte";
   import Demo3 from "./Demo3.svelte";
+  import Demo4 from "./Demo4.svelte";
   export let title;
 
   // console.log(container);
@@ -30,6 +31,7 @@
     <Demo1 />
     <Demo2 />
     <Demo3 />
+    <Demo4 />
   </main>
   <aside>
     <ul class="content">
